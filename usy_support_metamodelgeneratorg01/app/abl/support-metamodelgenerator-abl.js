6378b84d03e2073913519e773e26e6500094d574
Jiri Neuman <jiri.neuman@gmail.com>
2023-04-11 13:48:30 +0200

    Added support for metamodel schema 2.0 (#4)
    Co-authored-by: Jiri Neuman <jiri.neuman@unicorn.com>

--- usy_support_metamodelgeneratorg01/app/abl/support-metamodelgenerator-abl.js
+++ usy_support_metamodelgeneratorg01/app/abl/support-metamodelgenerator-abl.js
@@ -2,19 +2,29 @@
 const fs = require("fs");
 const path = require("path");
 const IGNORED_PROFILES = ["AwidOwner", "Public", "AwidLicenseOwner"];
+const FILE_READ_OPTIONS = {encoding: "utf8", flag: "r"}
 
 class SupportMetamodelgeneratorAbl {
 
   async createMetaModel(dtoIn) {
-    let fileReadoptions = {encoding: "utf8", flag: "r"}
-    const profiles = JSON.parse(fs.readFileSync(dtoIn.profilesFile, fileReadoptions));
-    const template = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../", "resources/template.json"), fileReadoptions));
+    const profiles = JSON.parse(fs.readFileSync(dtoIn.profilesFile, FILE_READ_OPTIONS));
     let existingMetamodel;
     if (fs.existsSync(dtoIn.metamodel)) {
-      existingMetamodel = JSON.parse(fs.readFileSync(dtoIn.metamodel, fileReadoptions));
+      existingMetamodel = JSON.parse(fs.readFileSync(dtoIn.metamodel, FILE_READ_OPTIONS));
     }
-    let res = template;
 
+    let res;
+    if (existingMetamodel && (existingMetamodel.schemaVersion.startsWith("1") || existingMetamodel.schemaVersion.startsWith("0"))) {
+      res = this.createMetaModelV1(existingMetamodel, profiles, dtoIn.mandatoryProfiles);
+    } else {
+      res = this.createMetaModelV2(existingMetamodel, profiles, dtoIn.mandatoryProfiles);
+    }
+
+    fs.writeFileSync(dtoIn.metamodel, JSON.stringify(res, null, 2))
+  }
+  createMetaModelV1(existingMetamodel, profiles, mandatoryProfiles) {
+    const template = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../", "resources/template-1.0.json"), FILE_READ_OPTIONS));
+    let res = template;
     if (existingMetamodel) {
       this._fillHeader(res, existingMetamodel);
     }
@@ -31,7 +41,7 @@ class SupportMetamodelgeneratorAbl {
     }
     let profilesUcMap = profiles["*"].useCaseMap;
 
-    if (dtoIn.mandatoryProfiles.some(i => !(profileList.indexOf(i) > -1))) {
+    if (mandatoryProfiles.some(i => !(profileList.indexOf(i) > -1))) {
       throw "Missing mandatory profile. You must have at least these 3 profiles in your profiles.json : Authorities, Executives, Auditors. If you have these profiles with different name, please use --mandatory-profiles attribute to map it.";
     }
 
@@ -40,10 +50,10 @@ class SupportMetamodelgeneratorAbl {
       res.defaultPermissionMatrix = existingMetamodel.defaultPermissionMatrix;
     } else {
       res.profileList = [];
-      dtoIn.mandatoryProfiles.forEach(p => res.profileList.push(
+      mandatoryProfiles.forEach(p => res.profileList.push(
         {code: p, name: p, desc: p, disableImplicitPermissions: false, enabledExplicitTypeList: ["uu-businessterritory-maing01/uuRoleGroupIfc"]}));
       profileList.forEach(p => {
-        if (dtoIn.mandatoryProfiles.indexOf(p) < 0) {
+        if (mandatoryProfiles.indexOf(p) < 0) {
           res.profileList.push(
             {code: p, name: p, desc: p, disableImplicitPermissions: false, enabledExplicitTypeList: ["uu-businessterritory-maing01/uuRoleGroupIfc"]})
         }
@@ -60,7 +70,62 @@ class SupportMetamodelgeneratorAbl {
       res.useCaseProfileMap[key] = this._getProfilesMatrix(profiles, profilesIndex)
     });
 
-    fs.writeFileSync(dtoIn.metamodel, JSON.stringify(res, null, 2))
+    return res;
+  }
+
+  createMetaModelV2(existingMetamodel, profiles, mandatoryProfiles) {
+    const template = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../", "resources/template-2.0.json"), FILE_READ_OPTIONS));
+    let res = template;
+    if (existingMetamodel) {
+      this._fillHeader(res, existingMetamodel);
+    }
+
+    let profileList = profiles["*"].profileList;
+    profileList = profileList.filter(p => !IGNORED_PROFILES.includes(p));
+    if (existingMetamodel) {
+      if (!this._checkArrayEquals(profileList, existingMetamodel.roleGroupProfileList.map(p => p.code))) {
+        console.log("Profiles are not same !!!");
+        console.log(`profiles.json : ${profileList.join(", ")}`)
+        console.log(`metamodel.json: ${existingMetamodel.roleGroupProfileList.map(p => p.code).join(", ")}`)
+        return;
+      }
+    }
+    let profilesUcMap = profiles["*"].useCaseMap;
+
+    if (mandatoryProfiles.some(i => !(profileList.indexOf(i) > -1))) {
+      throw "Missing mandatory profile. You must have at least these 3 profiles in your profiles.json : Authorities, Executives, Auditors. If you have these profiles with different name, please use --mandatory-profiles attribute to map it.";
+    }
+
+    if (existingMetamodel) {
+      res.roleGroupProfileList = existingMetamodel.roleGroupProfileList;
+      res.roleProfileList = existingMetamodel.roleProfileList;
+      res.defaultPermissionMatrix = existingMetamodel.defaultPermissionMatrix;
+    } else {
+      res.roleGroupProfileList = [];
+      mandatoryProfiles.forEach(p => res.roleGroupProfileList.push(
+        {code: p, name: p, desc: p, disableImplicitPermissions: false, enabledExplicitTypeList: ["uu-businessterritory-maing01/uuRoleGroupIfc"]}));
+      profileList.forEach(p => {
+        if (mandatoryProfiles.indexOf(p) < 0) {
+          res.roleGroupProfileList.push(
+            {code: p, name: p, desc: p, disableImplicitPermissions: false, enabledExplicitTypeList: ["uu-businessterritory-maing01/uuRoleGroupIfc"]})
+        }
+      });
+    }
+
+    let profilesIndex = new Map();
+    res.roleGroupProfileList.forEach((p, i) => profilesIndex.set(p.code, i));
+
+    Object.keys(profilesUcMap).forEach(uc => {
+      let ucProfiles = profilesUcMap[uc];
+      let profiles = ucProfiles.profileList || ucProfiles;
+      let key = res.schemaVersion == "0.1.0" ? uc : `${res.code}/${uc}`;
+      res.useCaseProfileMap[key] = {
+        roleGroupProfileMaskList: [this._getProfilesMatrix(profiles, profilesIndex)],
+        roleProfileMaskList: existingMetamodel?.useCaseProfileMap[key]?.roleProfileMaskList || ["00000000-00000000-00000000-00000000"]
+      };
+    });
+
+    return res;
   }
 
   _getProfilesMatrix(profiles, profilesIndexMap) {
